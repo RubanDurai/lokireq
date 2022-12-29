c7175d2802fdb87328d8033efb8818a69d03660b
tempreqbot <121566384+tempreqbot@users.noreply.github.com>
2022-12-29 23:06:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -767,9 +767,12 @@ async def auto_filter(client, msg, spoll=False):
             fmsg = await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except Exception as e:
             logger.exception(e)
-            fmsg = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+            image = "https://telegra.ph/file/aafe62b4d442f7aceea20.jpg"
+            fmsg = await message.reply_photo(photo=image, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
     else:
-         fmsg = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
+         image = "https://telegra.ph/file/aafe62b4d442f7aceea20.jpg"
+        fmsg = await message.reply_photo(photo=image, caption=cap, reply_markup=InlineKeyboardMarkup(btn))
+    if spoll:
        
     await asyncio.sleep(900)
     await fmsg.delete()
