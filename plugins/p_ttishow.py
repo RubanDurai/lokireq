1ebb9f8ec6a731491d9ce7500a8ea03b4f22f025
tempreqbot <121566384+tempreqbot@users.noreply.github.com>
2022-12-29 11:04:27 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -59,7 +59,7 @@ async def save_group(bot, message):
                         pass
                 temp.MELCOW['welcome'] = await message.reply_video(
                                                  video=("https://telegra.ph/file/4cd1f928c53c8e1c47542.mp4"),
-                                                 caption=("<b>ʜᴇʏ {message.from_user.mention}, ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ Mᴏᴠɪᴇ ɢʀᴏᴜᴘ ❤️</b>"),
+                                                 caption=f"<b>Hey {message.from_user.mention}, Welcome to our Movie Group ❤️</b>"),
                                                  reply_markup=InlineKeyboardMarkup(
                                                                          [[
                                                                            InlineKeyboardButton('❗️Rᴇᴀᴅ Gʀᴏᴜᴘ Rᴜʟᴇꜱ❗️', url="https://telegra.ph/%F0%9D%90%91%F0%9D%90%9A%F0%9D%90%AC%F0%9D%90%A1%F0%9D%90%A2%F0%9D%90%A6%F0%9D%90%A2%F0%9D%90%A4%F0%9D%90%9A-11-26")
